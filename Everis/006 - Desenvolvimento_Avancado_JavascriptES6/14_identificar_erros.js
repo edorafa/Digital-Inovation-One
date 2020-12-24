56c0447e53eba6d275d6d5a3742d2db37984738a
Rafael Sevilha <54326621+edorafa@users.noreply.github.com>
2020-12-24 10:40:32 -0300

    Add files via upload

---
+++ Everis/006 - Desenvolvimento_Avancado_JavascriptES6/14_identificar_erros.js
@@ -0,0 +1,24 @@
+class CustomError extends Error {
+    constructor({message, data}){
+    super(message);
+    this.data = data;
+    }
+}
+
+try {
+    const myError = new CustomError({
+        message: 'Custom message',
+        data: {
+            type: 'Server error'
+        }
+    });
+    throw myError;
+}catch(err){
+    if(err.data.type === 'Server error'){
+        
+    }else {
+
+    }
+}finally {
+    console.log('Keep going...');
+}
---
+++ Everis/006 - Desenvolvimento_Avancado_JavascriptES6/15_debbuging.js
@@ -0,0 +1,23 @@
+
+
+
+/*
+protocolo http: versao 1 ou 2
+
+Principais diferenças:
+ -> performance
+ -> forma de transferencia dos dados
+
+conteúdo é o mesmo
+localhost corresponde ao html
+
+
+Localhost: metadados, cabeçalho e um corpo
+
+General: URL, method, status
+
+Headers de resposta relacionados a cach
+Request Headers: tipos de dados que aceita, 
+idioma e informações sobre user Agent
+
+*/
